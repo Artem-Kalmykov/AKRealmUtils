b5707537bdc397c925bb90be63792524e4aa30f7
Artem Kalmykov <ar.kalmykov@gmail.com>
2019-12-11 17:41:22 +0200

    Add SPM dependency.

--- Package.swift
+++ Package.swift
@@ -12,8 +12,10 @@ let package = Package(
             targets: ["AKRealmUtils"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(
+            url: "https://github.com/realm/realm-cocoa.git",
+            .branch("master")
+        )
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
